e8c8b08fa94351f9a8fa1d0ab38416ea2c6d2e68
Cavasso <victorcavasso@hotmail.com>
2021-08-24 23:48:36 -0300

    until08Class

--- index.js
+++ index.js
@@ -2,19 +2,23 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
-const {convert, toMoney} = require('./lib/convert')
+const { convert, toMoney } = require('./lib/convert')
+const apiBCB = require('./lib/api.bcb')
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', (req, res) =>{
-    res.render('home')
+app.get('/', async (req, res) => {
+    const cotacao = await apiBCB.getCotacao()
+    res.render('home', {
+        cotacao
+    })
 })
 app.get('/cotacao', (req, res) => {
-    const {cotacao, quantidade} = req.query
-    
-    if(cotacao && quantidade){
+    const { cotacao, quantidade } = req.query
+
+    if (cotacao && quantidade) {
         const conversao = convert(cotacao, quantidade)
         res.render('cotacao', {
             error: false,
@@ -22,17 +26,17 @@ app.get('/cotacao', (req, res) => {
             quantidade: toMoney(quantidade),
             conversao: toMoney(conversao)
         })
-    }else{
+    } else {
         res.render('cotacao', {
             error: 'Valores inválidos'
         })
     }
 })
 
-app.listen(3000, err =>{
-    if(err){
+app.listen(3000, err => {
+    if (err) {
         console.log('Não foi possivel inicial o servidor')
-    }else{
+    } else {
         console.log('ConvertMyMoney está online')
     }
 })
