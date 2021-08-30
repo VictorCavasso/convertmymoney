022021b4aa631a503876d8b264528c1142fb1560
Cavasso <victorcavasso@hotmail.com>
2021-08-30 20:42:17 -0300

    Correção

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
-const { convert, toMoney } = require('./lib/convert')
+const convert = require('./lib/convert')
 const apiBCB = require('./lib/api.bcb')
 
 app.set('view engine', 'ejs')
@@ -19,12 +19,12 @@ app.get('/cotacao', (req, res) => {
     const { cotacao, quantidade } = req.query
 
     if (cotacao && quantidade) {
-        const conversao = convert(cotacao, quantidade)
+        const conversao = convert.convert(cotacao, quantidade)
         res.render('cotacao', {
             error: false,
-            cotacao: toMoney(cotacao),
-            quantidade: toMoney(quantidade),
-            conversao: toMoney(conversao)
+            cotacao: convert.toMoney(cotacao),
+            quantidade: convert.toMoney(quantidade),
+            conversao: convert.toMoney(conversao)
         })
     } else {
         res.render('cotacao', {
